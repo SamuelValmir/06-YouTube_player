d580a4e0bc05f146f3d86929d13f60b37c447ae8
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-08-20 18:08:27 -0300

    Project allocated to another folder.
    Code more clean with no repetition of variables declarations.
    
    Volume slider added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,42 @@
-var defaultScreen =  16/9;
+var defaultScreen = 16 / 9;
+var video;
+var volumeSlider;
 
-window.onload = function(){
+var autoplayImage;
+
+window.onload = function () {
+    //Inicialize the variables
+    video = document.getElementById("video");
+    volumeSlider = document.getElementsByClassName("volume_slider")[0];
+    autoplayImage = document.getElementById("autoplayImg");
     setProperties();
 }
 
-function setProperties(){ //Fill or change the div properties values
-    let video = document.getElementById("videoId");
-    
+function setProperties() { //Fill or change the div properties values
+
+
     document.getElementById("volume").innerHTML = video.volume;
     document.getElementById("muted").innerHTML = video.muted;
     document.getElementById("autoplay").innerHTML = video.autoplay;
     let playButtonImageSrc = document.getElementById("play_button").src;
 
     //If there is pause inside image button src ...
-    if (playButtonImageSrc.search("pause") > -1){
+    if (playButtonImageSrc.search("pause") > -1) {
         document.getElementById("playing").innerHTML = "true";
-    } else if (playButtonImageSrc.search("play") > -1){
+    } else if (playButtonImageSrc.search("play") > -1) {
         document.getElementById("playing").innerHTML = "false";
-    } else{
+    } else {
         alert("error on muted image")
     }
 }
 
+function onSliderInputted() {
+    video.volume = volumeSlider.value;
+    setProperties();
+}
 
 function playVideo() { //Play or pause the video
     let playButton = document.getElementById("play_button");
-    let video = document.getElementById("videoId");
 
     //If video is paused...
     if (video.paused) { // ... play it, change play button title and play button image
@@ -38,15 +49,15 @@ function playVideo() { //Play or pause the video
         playButton.title = "Play (k)";
         playButton.setAttribute("src", "./assets/icons/round_play_arrow_black.png");
     }
+    setProperties();
 }
 
 function muteVideo() { // Mutes or un-mutes the volume
-    let video = document.getElementById("videoId");
     let muteButton = document.getElementById("mute_button");
 
     //If video's volume is 0...
     if (video.muted) { //... change image and set muted as false
-        if(video.volume == 0){
+        if (video.volume == 0) {
             video.volume = 1;
             setVolumeImage();
             video.muted = false;
@@ -60,31 +71,32 @@ function muteVideo() { // Mutes or un-mutes the volume
         video.muted = true;
     }
     setProperties();
+    setVolumeRangeByVolumeMuted(video.muted);
 
 }
 
 function adjustVolume(direction) {
-    let video = document.getElementById("videoId");
-
+    //If user press arrow up and video's volume is lower than 1...
     if (direction == "up" && video.volume.toFixed(1) < 1) {
         video.volume = (video.volume + 0.1).toFixed(1);
     }
 
+    //If user press arrow down and video's volume is greater than 1...
     if (direction == "down" && video.volume.toFixed(1) > 0) {
         video.volume = (video.volume - 0.1).toFixed(1);
     }
+
     setVolumeImage();
+    setVolumeRange();
 }
 
 function setVolumeImage() { //Define volume image depending to video's volume level
-    let video = document.getElementById("videoId");
-    let volume = video.volume;
     let muteButton = document.getElementById("mute_button");
 
-    if (volume == 0) {
+    if (video.volume == 0) {
         muteButton.src = "./assets/icons/round_volume_off_black.png";
         video.muted = true;
-    } else if (volume <= 0.5) {
+    } else if (video.volume <= 0.5) {
         muteButton.src = "./assets/icons/round_volume_down_black.png";
     } else {
         muteButton.src = "./assets/icons/round_volume_up_black.png";
@@ -94,8 +106,19 @@ function setVolumeImage() { //Define volume image depending to video's volume le
 
 }
 
+function setVolumeRange() {
+    volumeSlider.value = video.volume;
+}
+
+function setVolumeRangeByVolumeMuted(isVideoMuted) {
+    if (isVideoMuted) {
+        volumeSlider.value = 0;
+        return;
+    }
+    volumeSlider.value = video.volume;
+}
+
 window.onkeydown = function () { //Depending to which key was pressed down, it'll control different properties of the video.
-    let video = document.getElementById("videoId");
     const keyDown = event.keyCode;
     // alert(keyDown)
     switch (keyDown) {
@@ -112,7 +135,7 @@ window.onkeydown = function () { //Depending to which key was pressed down, it'l
         case 40: adjustVolume("down"); //When Arrow down is pressed
             break;
         case 65: setAutoPlay(); //When "A" is pressed
-        break;
+            break;
         case 70: setFullScreen(); //When "F" is pressed
             break;
         case 77: muteVideo(); //When "M" is pressed
@@ -121,8 +144,7 @@ window.onkeydown = function () { //Depending to which key was pressed down, it'l
 };
 
 function setAutoPlay() {
-    let video = document.getElementById("videoId");
-    let autoplayImage = document.getElementById("autoplayImg");
+
     // If video autoplay is true ...
     if (video.autoplay) { // ...make it false, change autoplay image and it's title.
         video.autoplay = false;
@@ -152,7 +174,6 @@ function setTheater() { // Turn in or out in theater mode
         container.style.width = "100%";
 
         theaterImage.src = "./assets/icons/round_crop_7_5_black.png"
-        let video = document.getElementById("videoId");
     }
 }
 
@@ -172,6 +193,5 @@ function setFullScreen() { // Turn in or out in full screen mode
         container.style.width = "100%";
         container.style.height = "100%";
         fullScreenImage.src = "./assets/icons/round_fullscreen_exit_black.png"
-        let video = document.getElementById("videoId");
     }
 }
